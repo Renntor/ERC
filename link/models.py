ac3fef100b8b7ceb690daeda9e10b27664548b80
ren <denis30510@gmail.com>
2023-12-24 18:14:13 +0300

    added migrations

--- link/models.py
+++ link/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+NULLABLE = {'null': True, 'blank': True}
+
 
 class Link(models.Model):
     name = models.CharField(max_length=150, verbose_name='название')
@@ -10,3 +12,8 @@ class Link(models.Model):
     street = models.CharField(max_length=100, verbose_name='улица')
     number = models.CharField(max_length=10, verbose_name='номер')
     house = models.CharField(max_length=50, verbose_name='дом')
+    debt = models.FloatField(default=None, verbose_name='долг', **NULLABLE)
+    create = models.DateTimeField(auto_now=True, verbose_name='время создания')
+    factory = models.BooleanField(verbose_name='завод')
+
+    supplier = models.ForeignKey('self', on_delete=models.SET_NULL, **NULLABLE, verbose_name='поставщик')
--- product/models.py
+++ product/models.py
@@ -1,14 +1,12 @@
 from django.db import models
-from link.models import Link
-
-NULLABLE = {'null': True, 'blank': True}
+from link.models import Link, NULLABLE
 
 
 class Product(models.Model):
-    link = models.ForeignKey(Link, on_delete=models.CASCADE, verbose_name='компания', **NULLABLE)
+    link = models.ForeignKey(Link, on_delete=models.SET_NULL, verbose_name='компания', **NULLABLE)
     name = models.CharField(max_length=100, verbose_name='название')
     model = models.CharField(max_length=100, verbose_name='модель')
-    release_date = models.DateField(verbose_name='дата выхода', auto_now=True)
+    release_date = models.DateField(verbose_name='дата выхода')
 
     def __str__(self):
         return f'{self.name}, {self.model}, {self.release_date}'
